d85958f8e0b973a047124046e2d442ba8dd4ab98
t3ffsung <mrsecret11118@gmail.com>
2025-06-15 22:21:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,2 @@
-importScripts("jokes.js");
-
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  setInterval(() => {
-    if (Notification.permission === 'granted') {
-      self.registration.showNotification("😂 Sarcastic Joke", {
-        body: getRandomJoke(),
-        icon: 'icon.png'
-      });
-    }
-  }, 5 * 60 * 1000); // every 5 minutes
-});
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
